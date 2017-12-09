00b73ab9c3a20c76e09dfd5febdc1b8dec7b8ac0
ShashkovAA <wawulya2@yandex.ru>
2017-12-09 20:10:04 +0300

    Pinger1.3. Exec command, coding, log4j.xml file location set in args

--- src/Run/Pinger.java
+++ src/Run/Pinger.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,26 +14,36 @@ import Objects.MyCalendar;
 public class Pinger
 {
 	private static Logger log;
+	private static String command = "";
+	private static String coding = "";
 	
-	public Pinger(String address) {
-		ping(address);		
+	public Pinger() {
+		ping();		
 	}
 	
 	
 	public static void main(String[] args){
 		File file = new File("./logs");
 	    file.mkdir();
-		System.out.println("Programm started "+ MyCalendar.getCurrentDate() + " " + MyCalendar.getCurrentTimeNowWithSS());
+	    	    
+	    for (int i=0; i<args.length; i++) {
+	    	if (args[i].startsWith("coding="))
+	    		coding = args[i];
+	    	else
+	    		command += args[i] + " ";
+	    }
+		
+	    System.out.println("Programm started "+ MyCalendar.getCurrentDate() + " " + MyCalendar.getCurrentTimeNowWithSS());
 		if (args.length == 0){
-			System.out.println("No specify address for 'ping' command! ");
-			System.out.println("Set the address as an argument by start programm.");
+			System.out.println("No specify command! ");
+			System.out.println("Set the full command as an argument by start programm.");
 			pressAnyKeyToContinue();
 			
 		}
 		else {	
-		System.out.println("Execute: ping -t "+ args[0]);
+		System.out.println("Execute: " + command);
 		System.out.println("Output writing to /logs/DebugLog.txt");
-		Pinger pinger1 = new Pinger(args[0]);
+		Pinger pinger1 = new Pinger();
 		}		
 	}
 	 
@@ -42,14 +53,20 @@ public class Pinger
 	    return log;
 	 }
 	
-	private void ping(String address) {
+	private void ping() {
 
-        String pingCmd = "ping " + address + " -t ";
-        try {
+        if (coding.isEmpty())
+        	coding = "Cp1251";
+        else 
+        	coding = coding.substring(7);
+        
+        getLogger().info("Coding = [" + coding + "]");
+        
+		try {
             Runtime r = Runtime.getRuntime();
-            Process p = r.exec(pingCmd);
+            Process p = r.exec(command);
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream(),"Cp1251"));
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream(),coding));
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
                 getLogger().debug(inputLine);
